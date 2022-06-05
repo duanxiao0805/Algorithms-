ae829cd8f1d67d193af77961f186b100a69e722f
duaoxxxxx <62934005+duanxiao0805@users.noreply.github.com>
2022-06-05 15:45:04 +0800

    Update 1.3.1.java

--- 1.3背包、队列和栈(课后练习)/1.3.1.java
+++ 1.3背包、队列和栈(课后练习)/1.3.1.java
@@ -1,1 +1,33 @@
+package Main;
+/*
+ * 为FixedCapacityStackOfStrings添加一个方法:Isfull()
+ */
+class FixedCapacityStackOfStrings {
+    private String[] a;
+    private int N;
 
+    public FixedCapacityStackOfStrings(int cap) {
+        a = new String[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(String item) {
+        a[N++] = item;
+    }
+
+    public String pop() {
+        return a[--N];
+    }
+
+    // 添加IsFull()方法
+    public boolean Isfull() {
+        return N == a.length;
+    }
+}
